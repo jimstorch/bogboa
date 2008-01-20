1e202bd981d9dc93f48c1b02a81fa502699558da
jimstorch <jimstorch@d70f343f-5343-0410-acca-e5f3a4fc5206>
2008-01-20 23:21:58 +0000

    made some nice progress on the parsing system

--- trunk/ruleset/abilities/__init__.py
+++ trunk/ruleset/abilities/__init__.py
@@ -1,12 +1,16 @@
 #------------------------------------------------------------------------------
-#   File:       abilty/__init__.py
-#   Purpose:    mapping of player commands to functions
+#   File:       ruleset/abilities/__init__.py
+#   Purpose:    initialization of stuff needed
 #   Author:     Jim Storch
 #------------------------------------------------------------------------------
 
 from server import shared
 from ruleset.abilities import speech
 
-shared.ABILITY_DICT['tell'] = speech.tell
-
-
+shared.ABILITY_DICT['tell'] = (speech.tell.parser, speech.tell)
+shared.ABILITY_DICT['t'] = (speech.tell.parser, speech.tell)
+shared.ABILITY_DICT['whisper'] = (speech.tell.parser, speech.tell)
+shared.ABILITY_DICT['w'] = (speech.tell.parser, speech.tell)
+shared.ABILITY_DICT['reply'] = (speech.reply.parser, speech.reply)
+shared.ABILITY_DICT['r'] = (speech.reply.parser, speech.reply)
+shared.ABILITY_DICT['shout'] = (speech.shout.parser, speech.shout)
