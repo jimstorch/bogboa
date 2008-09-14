f9605b76e9357d692ca928baa74555aef992d942
jimstorch <jimstorch@d70f343f-5343-0410-acca-e5f3a4fc5206>
2008-09-14 21:23:42 +0000

    structural change - heaving breakage

---
+++ lib/player.py
@@ -0,0 +1,26 @@
+#------------------------------------------------------------------------------
+#   File:       player.py
+#   Author:     Jim Storch
+#------------------------------------------------------------------------------
+
+
+class Player(object):
+
+    def __init__(self):
+
+        ## Identity
+        self.uuid = None
+        self.name = None
+        self.race = None
+        self.gender = None
+        self.archetype = None
+        self.level = None
+        self.skill_map = {}
+
+        self.room = None
+        
+        self.friendly_target = None
+        self.hostile_target = None        
+
+
+    
